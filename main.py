2deded1753b708869a631c3a30caf118105f08f7
Pranit Srivastava <167555109+prontoexists@users.noreply.github.com>
2025-05-10 23:46:21 -0500

    Update main.py
    Finished commands etc. (coded from pycharm)

--- main.py
+++ main.py
@@ -3,43 +3,161 @@ import os
 import struct
 
 bSize = 512
-eBytVAL = b'4348PRJ3'
+eBytVal = b'4348PRJ3'
 headVAL = '>8sQQ'
 
 def create_index(filename):
+
     if os.path.exists(filename):
-        print(f"Error:file already exists.")
+
+        print(f"Error: file already exists.")
         return
 
-    with open(filename, 'wb') as f:
-        header = struct.pack(headVAL, eBytVAL, 0, 1)
-        padding = b'\x00' * (bSize - len(header))
-        f.write(header + padding)
+    try:
+        with open(filename, 'wb') as f:
 
-    print(f"Index file '{filename}' created successfully.")
-def main():
-    if len(sys.argv) < 3:
-        print("Usage: project3 <command> <filename> [args...]")
-        return
+            header = struct.pack(headVAL, eBytVal, 0, 1)
+            padding = b'\x00' * (bSize - len(header))
+            f.write(header + padding)
 
-    command = sys.argv[1]
-    filename = sys.argv[2]
+        print(f"file created successfully.")
+
+    except Exception as e:
+
+        print(f"Error creating file ")
+
+def insert_key(filename, key, value):
+
+    from btree import insert_key
+
+    try:
+
+        insert_key(filename, key, value)
+        print(f"Inserted key={key}, value={value} into {filename}.")
+
+    except Exception as e:
+
+        print(f"Error inserting into '{filename}': {e}")
+
+def search_key(filename, key):
+
+    from btree import search_key
+    try:
+
+        search_key(filename, key)
+
+    except Exception as e:
+
+        print(f"Error searching")
+
+def print_tree(filename):
+
+    from btree import print_tree
+    try:
+        print_tree(filename)
+
+    except Exception as e:
+
+        print(f"Error printing ")
+
+def load_csv(filename, csvfile):
+
+    from btree import load_csv
+    try:
+
+        load_csv(filename, csvfile)
+        print(f"Data successfully loaded")
+
+    except Exception as e:
+
+        print(f"Error loading CSV file ")
+
+def extract_csv(filename, output):
+
+    from btree import extract_csv
+    try:
+
+        extract_csv(filename, output)
+        print(f"Data extracted")
+
+    except Exception as e:
+        print(f"Error extracting data ")
+
+def handle_command(command_args):
+
+
+    command = command_args[0]
+    filename = command_args[1]
 
     try:
         if command == "create":
+
             create_index(filename)
 
-       #commands go here
+        elif command == "insert":
+
+            if len(command_args) < 4:
+                print("Error insert needs a key and a value.")
+                return
+
+            key = int(command_args[2])
+            value = int(command_args[3])
+            insert_key(filename, key, value)
+
+        elif command == "search":
+
+            if len(command_args) < 3:
+                print("Error: search needs a key.")
+                return
+
+            key = int(command_args[2])
+            search_key(filename, key)
+
+        elif command == "print":
+            print_tree(filename)
+
+        elif command == "load":
+
+            if len(command_args) < 3:
+                print("Error load requires csv")
+                return
+
+            csvfile = command_args[2]
+            load_csv(filename, csvfile)
+
+        elif command == "extract":
+
+            if len(command_args) < 3:
+                print("Error: 'extract' requires output")
+                return
+
+            output = command_args[2]
+            extract_csv(filename, output)
 
         else:
-            print(f"Unknown")
+            print(f" error Unknown command")
 
-    except IndexError:
-        print("Error: Missing arg")
     except ValueError:
-        print("Error: Integers only")
+        print("Error: Key and value must be integers.")
     except Exception as e:
-        print(f"Error: Exception")
+        print(f"Error: {e}")
+
+def main():
+
+    print("Format: create file | search (file) ____ | insert (file) ____ | load (file) ___ | print (file) | extract (file) (other file)" )
+    print("Type 'm' to exit.\n")
+
+    while True:
+
+        user_input = input("Enter command: ").strip()
+
+        if user_input.lower() == "m":
+
+            print("Exit")
+            break
+
+        command_args = user_input.split()
+        handle_command(command_args)
 
 if __name__ == "__main__":
     main()
